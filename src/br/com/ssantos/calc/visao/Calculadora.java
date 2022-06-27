01762588b994fae3e9bd32de29e9d1b451cf2e7b
Sandro Santos <sandro.ads.dev@gmail.com>
2022-06-27 20:19:01 -0300

    Projeto calculadora

---
+++ src/br/com/ssantos/calc/visao/Calculadora.java
@@ -0,0 +1,15 @@
+package br.com.ssantos.calc.visao;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+	public Calculadora() {
+
+	}
+
+	public static void main(String[] args) {
+		new Calculadora();
+	}
+}
